92d21263507e5c263ebb49c56d3794ac5d428726
dcusend <dcusendmail@gmail.com>
2022-02-22 19:54:33 -0500

    Added Data Driven Test Case for App 742.

---
+++ Test Suites/Temp.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Temp</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9dc19b55-e8cf-47b8-a693-7274d295cc13</testSuiteGuid>
+   <testCaseLink>
+      <guid>78e54175-01f3-4983-8ede-31871f67edee</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Admin Suite Bootstrap/VT_TestCases/SaleKeyboard_TP_NoCF_App742_Payment_TC</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
