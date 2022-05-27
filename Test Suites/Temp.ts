2ffe7689ed50defd4c81b84e152ca6bb352f3b55
imtiaz <imtiaz@DESKTOP-B2QI6NS.govolution.com>
2022-05-27 18:39:33 -0400

    Update Temp Test Suite

--- Test Suites/Temp.ts
+++ Test Suites/Temp.ts
@@ -4,11 +4,17 @@
    <name>Temp</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <mailRecipient></mailRecipient>
+   <mailRecipient>iahmed@govolution.com;</mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>9dc19b55-e8cf-47b8-a693-7274d295cc13</testSuiteGuid>
+   <testCaseLink>
+      <guid>aad94f30-8dfa-4a77-8d61-6d6e1c1f04e7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Admin Suite Bootstrap/VT_TestCases/SaleKeyboard_TP_NoCF_Generic_Payment_TC</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
