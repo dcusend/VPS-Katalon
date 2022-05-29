b306d203cebf5f935fd678123c85d4caa31e6200
imtiaz <imtiaz@DESKTOP-B2QI6NS.govolution.com>
2022-05-28 22:15:37 -0400

    Updated Temp Test Suite.

--- Test Suites/Temp.ts
+++ Test Suites/Temp.ts
@@ -12,9 +12,9 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>9dc19b55-e8cf-47b8-a693-7274d295cc13</testSuiteGuid>
    <testCaseLink>
-      <guid>aad94f30-8dfa-4a77-8d61-6d6e1c1f04e7</guid>
+      <guid>f2d94542-bbcd-4566-984e-3318978fccec</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Admin Suite Bootstrap/VT_TestCases/SaleKeyboard_TP_NoCF_Generic_Payment_TC</testCaseId>
+      <testCaseId>Test Cases/Admin Suite Bootstrap/VT-NotInTestSuite/SaleKeyboard_TP_NoCF_App742_Payment_TC</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
