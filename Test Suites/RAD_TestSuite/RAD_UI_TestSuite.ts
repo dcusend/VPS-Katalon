db4383f4662d952a7283828615770bd1583efbd7
imtiaz <imtiaz@DESKTOP-B2QI6NS.govolution.com>
2023-02-17 16:46:23 -0500

    No Major changes.  Committed thru desktop for laptop.

--- Test Suites/RAD_TestSuite/RAD_UI_TestSuite.ts
+++ Test Suites/RAD_TestSuite/RAD_UI_TestSuite.ts
@@ -14,7 +14,7 @@
    <testCaseLink>
       <guid>c356db70-d17a-46bd-b756-7828dc2bb82e</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/UI_TestCases/LandingPage_UI_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
@@ -26,19 +26,19 @@
    <testCaseLink>
       <guid>ab09e508-024b-47a0-9429-3d86d3e7ffdb</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/UI_TestCases/PersonalIncomeTax_UI_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>9088f2e0-030e-4671-999e-d52bfdb5a312</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/UI_TestCases/ExtensionPayments_UI_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>932e4bfd-5d83-42e7-9a56-22cebf600b19</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/UI_TestCases/WithholdingTax_UI_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
@@ -50,55 +50,55 @@
    <testCaseLink>
       <guid>6fa752cd-82c7-4bf4-ab2e-78d0997b8a10</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/UI_TestCases/SaleAndUseMarketplaceFacilitator_UI_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>1d188679-70f8-47d4-852a-274a01aa253f</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/UI_TestCases/SaleAndUseMDForm202_UI_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>6331f1c0-c005-4d42-aa36-56f4fb4b8a37</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/UI_TestCases/ExistingLiability_UI_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>379dfbff-676c-440d-9480-01ed1a956b1d</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/UI_TestCases/VerifyServiceFeeVerbiage_UI_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>230efd0a-8566-44da-8547-691d5df51c4e</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/MRF_TestCases/EstimatedTax_MRF_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>6bb53584-c589-4dc9-b608-4383c8754b4c</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/MRF_TestCases/PersonalIncomeTax_MRF_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>955bbc15-3728-4cf3-b839-690a79dcc54f</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/MRF_TestCases/ExtensionPayments_MRF_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>d0b15b02-2cbd-47e7-bfdb-76e971c0a3fc</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/MRF_TestCases/SaleAndUse_MRF_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>4a0acb18-2969-4efb-b7ea-481db989c242</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/MRF_TestCases/WithholdingTax_MRF_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
@@ -110,49 +110,49 @@
    <testCaseLink>
       <guid>d439f9bf-d810-4c22-8b00-26f95155f245</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/MRF_TestCases/EstimatedTax_Joint_MRF_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>823e2b34-fea5-4e50-9883-cb13535a7791</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/Negative_TestCases/EMailNoMatch</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>2ce37bc6-9dfa-43c0-8085-32f46accb929</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/Negative_TestCases/TaxPayerSSNNoMatch</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>c0eca099-98dd-4ef7-b1a8-a41d9eb08888</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/Negative_TestCases/JointFilerSSNNoMatch</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>fc2732d4-1cb6-4ab1-bce6-112471d3eb53</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/Negative_TestCases/SSNjointFilerMoreThan9Error</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>cf74b9ca-b235-47c4-87f0-3fd97768f099</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/Negative_TestCases/SSNmoreThan9Error</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>7aa4c71f-4211-49ec-945a-3f3b4859723a</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/Negative_TestCases/FEINmoreThan9Error</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>70dd97cb-e845-4603-b6cf-5b0f79d2783f</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/Negative_TestCases/EL-NumberValidation</testCaseId>
    </testCaseLink>
    <testCaseLink>
@@ -170,7 +170,7 @@
    <testCaseLink>
       <guid>0606cb29-1498-4fb4-a593-b2a7555a3cac</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/PaymentsHardCoded/SAU_MDForm202_AfterJuly2021_HC_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
@@ -182,7 +182,7 @@
    <testCaseLink>
       <guid>27b5c73f-94eb-441f-9a00-e607b4414799</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/PaymentsHardCoded/SAU_MDForm202F_AfterJuly2021_MarketplaceFacilitator_HC_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
@@ -200,13 +200,13 @@
    <testCaseLink>
       <guid>a831393a-22b4-487f-93c1-afd81c75ee8c</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/PaymentsHardCoded/SAU_MDForm202_AfterJuly2021_OutOfStateVendor_HC_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>2e95f3ca-6153-49f8-a91e-4efbcdb07201</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RAD_TestCases/PaymentsHardCoded/SAU_MDForm202F_AfterJuly2021_BothChkBox_HC_TC</testCaseId>
    </testCaseLink>
    <testCaseLink>
