40dee79d770891a3dec883feb131d14250ac2254
Imtiaz-bitbucket <imtiazster@gmail.com>
2024-08-30 22:29:02 -0400

    Added Object Repository for VRelay Bootstrap Pages.

---
+++ Object Repository/IWP_Bootstrap/Page_PaymentEntryCC_Bootstrap/select_MM.rs
@@ -0,0 +1,204 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_MM</name>
+   <tag></tag>
+   <elementGuidId>849367fb-da41-447d-a0ea-27d5fd2a6ede</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>select[name=&quot;cardExpMonth&quot;]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//select[@name='cardExpMonth']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>select</value>
+      <webElementGuid>ca0c3900-4b4c-426a-aa06-2787c59c7ee8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>cardExpMonth</value>
+      <webElementGuid>ed55f24f-c4df-488e-8e9d-d944275640b2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>form-control</value>
+      <webElementGuid>881cefac-4679-4297-a86e-303fa7db9424</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>alt</name>
+      <type>Main</type>
+      <value>Expiration Date</value>
+      <webElementGuid>d6b024d2-a204-4e54-8df2-c26d5cdf59b3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tabindex</name>
+      <type>Main</type>
+      <value>1</value>
+      <webElementGuid>4c9e9db9-10c6-4014-9b0c-d2b23cd4a48a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+									MM
+										
+									01
+						
+									02
+						
+									03
+						
+									04
+						
+									05
+						
+									06
+						
+									07
+						
+									08
+						
+									09
+						
+									10
+						
+									11
+						
+									12
+						
+								</value>
+      <webElementGuid>f7cd06cf-9eac-448d-943e-b4c86c5a7e78</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;process&quot;)/div[@class=&quot;row&quot;]/div[@class=&quot;col-md-6 vertical-dividing-line&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-md-6&quot;]/div[@class=&quot;row form-group&quot;]/fieldset[1]/div[@class=&quot;col-md-5 col-sm-5 col-xs-5&quot;]/select[@class=&quot;form-control&quot;]</value>
+      <webElementGuid>2d04c6de-43df-4942-9734-8d78a7e81ccb</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//select[@name='cardExpMonth']</value>
+      <webElementGuid>9306a8c8-3ea4-42f1-98e4-8deddbed1865</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='process']/div/div/div[4]/div[2]/div/fieldset/div/select</value>
+      <webElementGuid>f6f6757b-a9b4-4914-b973-844c605e3966</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='*'])[4]/following::select[1]</value>
+      <webElementGuid>aa442f9a-8bf7-40af-8f18-d969c595787f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='*'])[5]/preceding::select[2]</value>
+      <webElementGuid>ecb8eb16-cf64-420a-8549-e930fc7955a0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//select</value>
+      <webElementGuid>bb0d63bf-d853-41f4-87c7-220425bb7510</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//select[@name = 'cardExpMonth' and @alt = 'Expiration Date' and (text() = '
+									MM
+										
+									01
+						
+									02
+						
+									03
+						
+									04
+						
+									05
+						
+									06
+						
+									07
+						
+									08
+						
+									09
+						
+									10
+						
+									11
+						
+									12
+						
+								' or . = '
+									MM
+										
+									01
+						
+									02
+						
+									03
+						
+									04
+						
+									05
+						
+									06
+						
+									07
+						
+									08
+						
+									09
+						
+									10
+						
+									11
+						
+									12
+						
+								')]</value>
+      <webElementGuid>6a3442d9-772f-4003-a0f7-0adbdf57ef9a</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/IWP_Bootstrap/Page_PaymentEntryCC_Bootstrap/select_YYYY.rs
@@ -0,0 +1,228 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_YYYY</name>
+   <tag></tag>
+   <elementGuidId>b50eefe9-fd59-454b-9aa3-0d0c9dfccedf</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>select[name=&quot;cardExpYear&quot;]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//select[@name='cardExpYear']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>select</value>
+      <webElementGuid>17ee809d-d14c-4e7c-9c02-f811d135fa8b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>cardExpYear</value>
+      <webElementGuid>b87a9713-83fd-46d3-ab68-cb88425d3824</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>form-control</value>
+      <webElementGuid>420a3313-3eea-4589-a175-010d77251d24</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>alt</name>
+      <type>Main</type>
+      <value>Expiration Date</value>
+      <webElementGuid>b29add5c-f6b2-4b4b-a49c-cbb2e40207e8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tabindex</name>
+      <type>Main</type>
+      <value>1</value>
+      <webElementGuid>1f328bb2-e1f2-455c-89b4-3ce88640be35</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+									YYYY
+						
+									2024
+						
+									2025
+						
+									2026
+						
+									2027
+						
+									2028
+						
+									2029
+						
+									2030
+						
+									2031
+						
+									2032
+						
+									2033
+						
+									2034
+						
+									2035
+						
+									2036
+						
+									2037
+						
+									2038
+						
+									2039
+						
+								</value>
+      <webElementGuid>7685c5b3-ddd7-4a2a-8cbe-53292b3489e0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;process&quot;)/div[@class=&quot;row&quot;]/div[@class=&quot;col-md-6 vertical-dividing-line&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-md-6&quot;]/div[@class=&quot;row form-group&quot;]/fieldset[1]/div[@class=&quot;col-md-7 col-sm-7 col-xs-7&quot;]/select[@class=&quot;form-control&quot;]</value>
+      <webElementGuid>248a8000-666d-4759-a699-006701c1808d</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//select[@name='cardExpYear']</value>
+      <webElementGuid>8b7f0d32-2ed0-4f78-9516-d200c9d54ca3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='process']/div/div/div[4]/div[2]/div/fieldset/div[2]/select</value>
+      <webElementGuid>830a8bde-107e-4ad9-8ee0-35c04e330d9a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='*'])[4]/following::select[2]</value>
+      <webElementGuid>fead6fd8-f92f-4b48-8e7f-0785d9e0bd53</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='*'])[5]/preceding::select[1]</value>
+      <webElementGuid>cd55ac89-f98d-449a-a3c8-b9ec7503f858</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/select</value>
+      <webElementGuid>5c8caaf2-e312-41ea-8fba-6d7631ceaf3a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//select[@name = 'cardExpYear' and @alt = 'Expiration Date' and (text() = '
+									YYYY
+						
+									2024
+						
+									2025
+						
+									2026
+						
+									2027
+						
+									2028
+						
+									2029
+						
+									2030
+						
+									2031
+						
+									2032
+						
+									2033
+						
+									2034
+						
+									2035
+						
+									2036
+						
+									2037
+						
+									2038
+						
+									2039
+						
+								' or . = '
+									YYYY
+						
+									2024
+						
+									2025
+						
+									2026
+						
+									2027
+						
+									2028
+						
+									2029
+						
+									2030
+						
+									2031
+						
+									2032
+						
+									2033
+						
+									2034
+						
+									2035
+						
+									2036
+						
+									2037
+						
+									2038
+						
+									2039
+						
+								')]</value>
+      <webElementGuid>a7382092-166e-479a-9bb9-dbb490d302e1</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
